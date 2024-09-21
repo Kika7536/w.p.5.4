f23d85df6d51e3f6411b8bdf6a82fae374b145e9
Kika7536 <104264433+Kika7536@users.noreply.github.com>
2024-09-21 16:03:04 +0300

    Добавьте файлы проекта.

---
+++ консоля.cpp
@@ -0,0 +1,77 @@
+﻿#include<iostream>
+using namespace std;
+
+void main() {
+	setlocale(LC_ALL, "rus");
+
+	int ticket, left = 0, right = 0, size = 6;
+	int counter = 0, tsize = 0;
+	
+	
+
+	cout << "Задание 4. Счастливый билетик." << endl << endl;
+	cout << "-------------------------------" << endl << endl;
+
+
+	
+//Программа будет считать адекватно только билеты с четным количеством цифр
+	
+
+
+
+	while (true) {
+		
+
+		cout << "Введите номер билета: ";
+		cin >> ticket;
+
+		counter = ticket;
+		while (counter > 0) {
+			counter /= 10;
+			tsize++;
+		}
+		
+		if (tsize != size) {
+			cout << "Ошибка. Номер билета должен состоять из " << size << " Символов. Повторите попытку" << endl;
+			tsize = 0;
+			continue;
+		}
+		else {
+			tsize = 0;
+		}
+
+
+
+
+
+		while (counter < (size / 2)) {
+			right += ticket % 10;
+			ticket /= 10;
+			counter++;
+
+		} counter = 0;
+		
+
+		while (counter < (size / 2)) {
+			left += ticket % 10;
+			ticket /= 10;
+			counter++;
+
+		}counter = 0;
+
+		
+
+		if (left == right) {
+			cout << "Поздравляю, билет счастливый !!!" << endl;
+		}
+		else {
+			cout << "Ничего! Повезет в следующий раз!" << endl;
+		}
+
+		left = 0;
+		right = 0;
+		
+	}
+
+
+}
